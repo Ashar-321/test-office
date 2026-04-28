1ec9816817acafdc852b9bc51ede38a737c9ddcf
Ashar <swishtagashar@gmail.com>
2026-04-28 17:32:21 +0500

    new

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\ArtWorkController;
+use App\Http\Controllers\ArtWorkController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
